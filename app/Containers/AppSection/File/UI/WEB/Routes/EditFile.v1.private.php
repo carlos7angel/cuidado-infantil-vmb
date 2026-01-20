1cb4deaeb2ea64c625d8cb03e2aeeb57fb6eb195
Carlos Alejo <carlos7angel@gmail.com>
2026-01-19 20:57:19 -0400

    remove & clean up FILE container

--- app/Containers/AppSection/File/UI/WEB/Routes/EditFile.v1.private.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Containers\AppSection\File\UI\WEB\Controllers\EditFileController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('files/{id}/edit', EditFileController::class)
-    ->middleware(['auth:web']);
-
--- app/Containers/AppSection/File/UI/WEB/Routes/StoreFile.v1.private.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Containers\AppSection\File\UI\WEB\Controllers\StoreFileController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('files/store', StoreFileController::class)
-    ->middleware(['auth:web']);
-
